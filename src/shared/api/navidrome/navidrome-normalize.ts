aded440cafe6e63fd95df10a773e9d588557c3a9
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-06-17 21:56:18 -0700

    use standard explicit status instead

--- src/shared/api/navidrome/navidrome-normalize.ts
+++ src/shared/api/navidrome/navidrome-normalize.ts
@@ -174,7 +174,7 @@ const normalizeSong = (
         discNumber: item.discNumber,
         discSubtitle: item.discSubtitle ? item.discSubtitle : null,
         duration: item.duration * 1000,
-        explicit: item.explicit || false,
+        explicit: item.explicitStatus === 'e',
         gain:
             item.rgAlbumGain || item.rgTrackGain
                 ? { album: item.rgAlbumGain, track: item.rgTrackGain }
@@ -243,7 +243,7 @@ const normalizeAlbum = (
         comment: item.comment || null,
         createdAt: item.createdAt.split('T')[0],
         duration: item.duration !== undefined ? item.duration * 1000 : null,
-        explicit: item.explicit || false,
+        explicit: item.explicitStatus === 'e',
         genres: (item.genres || []).map((genre) => ({
             id: genre.id,
             imageUrl: null,
